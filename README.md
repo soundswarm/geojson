f8a8d5d67d7c80528ecd0f3b014f2b21bd5c4fa2
Sean Connor <seantconnor@gmail.com>
2015-10-09 18:43:46 -0700

    API is fully operational.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# geojson
-Add a front end
+# Next Steps
+  add front end in React or Angular:
+    - Build responsive form that allows user to query database
+    - Display query results on a map and a list.  Map and List will have a real time filter.
+    - Add performance optimizations
+
+
+# geojson API
+- API endpoint URL is /listings
+
+API:
+GET /listings?min_price=100000&max_price=200000&min_bed=2&max_bed=2&min_bath=2&max_bath=2
+
+min_price: The minimum listing price in dollars.
+max_price: The maximum listing price in dollars.
+min_bed: The minimum number of bedrooms.
+max_bed: The maximum number of bedrooms.
+min_bath: The minimum number of bathrooms.
+max_bath: The maximum number of bathrooms.
+
+The expected response is a GeoJSON FeatureCollection of listings:
+
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-112.1,33.4]},
+      "properties": {
+  "id": "123ABC", # CSV id
+  "price": 200000, # Price in Dollars
+  "street": "123 Walnut St",
+        "bedrooms": 3, # Bedrooms
+        "bathrooms": 2, # Bathrooms
+        "sq_ft": 1500 # Square Footage
+    },
+    ...
+  ]
+}
+
--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+
+// server is using mongodb for the database hosted on heroku in mongolabs
 var mongo = require('mongodb');
 var mongoskin = require('mongoskin');
+
+// heroku and dev port
+var port = process.env.PORT || 3000;
+
+// heroku and dev db
 var db = mongoskin.db(process.env.MONGOLAB_URI || 'mongodb://@localhost:27017/geoJsonDb', {safe:true})
+
+// connection to db collection
 var collection = db.collection('geoJson', {strict: true});
 
+
+
+// converts GET fields to a database query
 var fieldsToQuery = function(fields) {
   var query = {};
   for(var field in fields) {
@@ -64,23 +75,48 @@ var fieldsToQuery = function(fields) {
   return query;
 };
 
+// convert an array of houses to geoJson
+var arrayToGeoJson = function(array) {
+  var features =  array.map(function(house) {
+    return {
+      "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [house.lng,house.lat]},
+      "properties": {
+        "id": house.id, // CSV id
+        "price": house.price, // Price in Dollars
+        "street": house.street,
+        "bedrooms": house.bedrooms, // Bedrooms
+        "bathrooms": house.bathrooms, // Bathrooms
+        "sq_ft": house.sq_ft // Square Footage
+      }
+    };
+  });
+  var geoJson = {
+    "type": "FeatureCollection",
+    "features": features
+  };
+  return geoJson;
+};
+
+
 // api controller
 var routeController = function(req, res) {
   var fields = req.query;
   var query =  fieldsToQuery(fields);
-  console.log(query);
   collection.find(query)
   .toArray(function(err, array) {
-    console.log(array);
-    res.status('200').send(array);
+    if(err) {
+      res.status('500').send(err);
+    } else {
+      var geoJson = JSON.stringify(arrayToGeoJson(array));
+      res.status('200').send(geoJson);
+    }
   });
-
 };
 
 // api route
 app.get('/listen', routeController);
 
-// https://s3.amazonaws.com/opendoor-problems/listings.csv
 
 app.listen(port);
 console.log('geojson is listening on port ' + port + '...');
